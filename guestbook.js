0d927a36be33edddd61303da75647b82a0a079f0
Kevin Oudai <46614071+Kevin-Oudai@users.noreply.github.com>
2021-11-16 18:44:35 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c06b8234.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
